166662fe733309ec58d37c1427acc75a236a4b39
Duobe <beduo@qq.com>
2019-03-06 21:42:40 +0800

    Component: Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,38 +1,45 @@
 import * as React from 'react'
 
 import styled from '../styles/styled-components'
+import noop from './../libs/noop'
 
-export interface ICardProps {
+export interface Props {
   className?: string
   checked?: boolean
   stacked?: boolean
+  children?: React.ReactNode
   onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 
-const Card: React.SFC<ICardProps> = ({ className, children, stacked = false, onClick }) => {
+export default function Card (props: Props) {
+  const { className, children, stacked = false, onClick = noop } = props
+
+  const onCardClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+
+    onClick()
+  }
+
   return (
-    <Wrapper className={className} stacked={stacked} onClick={onClick}>
+    <Wrapper className={className} stacked={stacked} onClick={onCardClick}>
       <Inner>{children}</Inner>
     </Wrapper>
   )
 }
 
-export default Card
-
 const Wrapper = styled.div<{ stacked: boolean }>(({ theme, stacked }) => ({
-  position: 'relative',
   '&::before': stacked
     ? {
-        position: 'absolute',
-        bottom: '-4px',
-        left: '5px',
-        right: '5px',
-        height: '6px',
-        content: '""',
-        background: theme.bgLighter,
-        boxShadow: theme.boxShadowLight,
-        borderRadius: theme.borderRadius
-      }
+      position: 'absolute',
+      bottom: '-4px',
+      left: '5px',
+      right: '5px',
+      height: '6px',
+      content: '""',
+      background: theme.bgLighter,
+      boxShadow: theme.boxShadowLight,
+      borderRadius: theme.borderRadius
+    }
     : '',
   '&:not(:last-child)': {
     marginBottom: '10px'
