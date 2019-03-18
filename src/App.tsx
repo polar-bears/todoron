ab084b03d15d54f6621d0df760a039c06aed4218
Duobe <beduo@qq.com>
2019-03-18 18:37:02 +0800

    Fix: gh-pages deployment problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import 'github-markdown-css'
+import 'highlight.js/styles/solarized-light.css'
+
 import * as React from 'react'
 import { ThemeProvider } from 'styled-components'
-import { createBrowserHistory } from 'history'
+import { createHashHistory } from 'history'
 import { Route, Router } from 'react-router'
 
 import MainView from './views/MainView'
 import { GlobalStyles } from './styles/reset'
 import { lightTheme } from './styles/theme'
 
-import 'github-markdown-css'
-import 'highlight.js/styles/solarized-light.css'
-
-const history = createBrowserHistory()
+const history = createHashHistory()
 const renderApp = (routeProps: any) => <MainView {...routeProps}/>
 
 export interface Props {}
